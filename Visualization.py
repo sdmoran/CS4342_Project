8556777242b73711c702c93197d482cca23b466a
pjmara <pjmarawork@gmail.com>
2019-12-07 10:30:34 -0500

    Pairwise plots v1

--- Visualization.py
+++ Visualization.py
@@ -9,6 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 # from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split, cross_val_score
+from PIL import Image
 
 data = pd.read_csv('./data/TrainData_Labeled.csv')
 
@@ -29,8 +30,59 @@ def classify(model, data):
     # print(classification_report(y_test, pred))
     scores = cross_val_score(model, X_test, y_test, cv=5)
     print("Accuracy: %0.2f (+/- %0.2f)\n" % (scores.mean(), scores.std() * 2))
+
+def performPlots(data):
+    X = np.asarray(data)[:, :11]
+    y = np.asarray(data)[:, 11]
+    dataToPlot = [] #array which holds arrays of each variable
+    for i in range(0, 11):
+        dataToPlot.append([])
+    dataToClass = []
+    for i in range(0, len(X)):
+        for j in range(0, len(X[i])):
+            dataToPlot[j].append(X[i][j])
+        dataToClass.append(y[i])
+        #plt.plot(datax, datay, 'x')
+    newArray = groupData(dataToPlot, dataToClass)
+    for arr in range(0, len(newArray)):
+        datax = [item[0] for item in newArray[arr]]
+        datay = [item[1] for item in newArray[arr]]
+        color = colorFromIndex(arr+3)
+        plt.plot(datax, datay, 'x', label=arr+3, color=color)
+    #plt.plot(dataToPlot[i], dataToPlot)
+    #plt.axis('equal')
+    plt.legend()
+    plt.savefig("pls.png")
+    plt.show()
+    #im = Image.open("pls.png") 
+    #im.show()
+
+def colorFromIndex(index):
+    if index == 3:
+        return '#ffcccc'
+    elif index == 4:
+        return '#99ff99'
+    elif index == 5:
+        return '#00ffff'
+    elif index == 6:
+        return '#ff5050'
+    elif index == 7:
+        return '#003366'
+    else:
+        return '#000000'
+
+def groupData(dataToPlot, dataToClass):
+    firstParam = dataToPlot[0]
+    secondParam = dataToPlot[1]
+    arr = []
+    for i in range(0, 6):
+        arr.append([])
+    for j in range(0, len(firstParam)):
+        arr[int(dataToClass[j])-3].append([firstParam[j], secondParam[j]])
+    #print(arr[1])
+    return arr
     
-# Heatmap correlations between different variables.
+Heatmap correlations between different variables.
 plt.figure(figsize=(12, 12))
 sns.heatmap(data=data.corr(), annot=True)
 
@@ -49,3 +101,4 @@ rfc = RandomForestClassifier(n_estimators=250)
 print("Random Forest classifier results:")
 classify(rfc, data)
 
+performPlots(data)
